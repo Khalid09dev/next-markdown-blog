72e7deb855afc53c40c84ff954fb9ee93e9e6913
khalid09dev <mdkhalidhossen@hotmail.com>
2024-10-07 16:35:20 +0600

    initialized nextjs project for a blog site

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+// About.js
+export default function About() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 font-sans p-5 shadow-lg rounded-lg">
+            <h1 className="text-4xl mb-4">About Us Page</h1>
+            <p className="text-lg text-center max-w-md">
+                This is the About Us Page. We are committed to providing the best service possible. Our team is dedicated to ensuring customer satisfaction and delivering high-quality products.
+            </p>
+        </div>
+    );
+}
